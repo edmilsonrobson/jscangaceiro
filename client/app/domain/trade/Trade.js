5dd8b64667feffc57688dc76080bd32aec2bbda2
Edmilson Rocha <edmilsonrobson@gmail.com>
2017-08-12 10:55:56 -0300

    Better immutability for the Trade classs

--- client/app/domain/trade/Trade.js
+++ client/app/domain/trade/Trade.js
@@ -1,13 +1,15 @@
 class Trade {
 
   constructor(date, quantity, value) {
-    this._date = date;
+    this._date = new Date(date.getTime());
     this._quantity = quantity;
     this._value = value;
+
+    Object.freeze(this);
   }
 
-  get volume() {
-    return getQuantity() * getValue();
+  get date() {
+    return new Date(this._date.getTime());
   }
 
   get quantity() {
@@ -16,6 +18,10 @@ class Trade {
 
   get value() {
     return this._value;
+  }
+
+  get volume() {
+    return this._quantity * this._value;
   }
 
 }
