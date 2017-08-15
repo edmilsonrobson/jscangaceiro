4b7dfefe14ff9e42b2dbe5ca95384a6360458c51
Edmilson Rocha <edmilsonrobson@gmail.com>
2017-08-15 20:32:56 -0300

    Added dynamic view update for TradeList

--- client/app/app.js
+++ client/app/app.js
@@ -1,3 +1,5 @@
 const controller = new TradeController();
 
 document.querySelector('.form').addEventListener('submit', controller.add.bind(controller));
+
+document.querySelector('#delete-button').addEventListener('click', controller.erase.bind(controller));
